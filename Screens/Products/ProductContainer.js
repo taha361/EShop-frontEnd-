659c427afe26c77289e5e1d76a2e105a3729239d
taha361 <tahayassinebenameur36@gmail.com>
2021-07-26 19:03:21 +0100

    first

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  Dimensions
+} from "react-native";
+import { Container, Header, Icon, Item, Input, Text } from "native-base";
+
+
+let { height } = Dimensions.get('window')
+
+const ProductContainer = (props) => {
+  const [products, setProducts] = useState([]);
+return (
+    <View>
+        aaaaaaa
+    </View>
+)
+
+}
+
+export default ProductContainer ;
